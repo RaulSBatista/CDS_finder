820c77decb729d1d87f81069a683db480ae95c10
RaulSBatista <98127955+RaulSBatista@users.noreply.github.com>
2022-02-05 15:07:41 -0300

    Update Blast.py

--- Blast.py
+++ Blast.py
@@ -1,39 +1,50 @@
 from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastnCommandline
-from Bio.Blast import NCBIWWW
+from Bio import SeqIO
+from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio.Blast import NCBIXML
 import sys
 import os
+import pandas as pd
 
 cepas = input('Digite a cepa: ')
 out = 'Blast'+cepas+'.xml'
-cepas = cepas+'.fasta'
+Cepas = cepas+'.fasta'
 
-cline = NcbiblastnCommandline(cmd = 'blastn', query = "C:/Users/raulS/Desktop/programas/Programa_Tobias/Regioes/All_regions.fasta", subject = cepas, outfmt = 5, out = out, reward = 1, penalty =-1, gapopen = 5, gapextend = 2)
+cline = NcbiblastnCommandline(cmd = 'blastn', query = "C:/Users/raulS/Desktop/programas/Programa_Tobias/Regioes/All_regions.fasta", subject = Cepas,outfmt = 5, out = out,reward = 1, penalty =-2, gapopen = 5, gapextend = 2)
 
 os.system((str(cline)))
 print(cline)
 
 out = out.removeprefix('xml')
 
+tabela = pd.read_excel('DATABASE.xlsx')
+
+lista_final = []
+
 with open(out,'r') as resultado:
     Regiao = 1
     for region, record in enumerate(NCBIXML.parse(resultado)):
         lista = []
-        print('REGION ' + str(region + 1) + '\n')
+        R = ('Region ' + str(region + 1) + '\n')
+        print(R)
+        Alig = 0
         for alignment in record.alignments:
-            Alig = 1
             for hsp in alignment.hsps:
                 align_len = hsp.align_length
-                FROM = hsp.sbjct_start
-                TO = hsp.sbjct_end
-                if align_len >= 1000:
-                    lista.append(f' Região: {Regiao}, Número alinhamento: {Alig} começa: {FROM}, termina: {TO}')
+                Subj_FROM = hsp.sbjct_start
+                Subj_TO = hsp.sbjct_end
+                Query_FROM = hsp.query_start
+                Query_TO = hsp.query_end
+                if align_len > 1000 or hsp.align_length >= 450 and hsp.expec == 0:
+                    Alig += 1
+                    #lista = (f'AL{Alig}(Query:{Query_FROM}...{Query_TO},Subject:{Subj_FROM}...{Subj_TO})'+'\n')
+                    lista = (f'{Subj_FROM}...{Subj_TO}')
+                    tabela.loc[f'Alinhamento: {Alig}', R] = lista
                     print(lista)
                     input()
-                Alig += 1
+            tabela.to_excel('DATABASE.xlsx')
         Regiao += 1
         lista_final.append(lista)
 
 print(lista_final)
-
